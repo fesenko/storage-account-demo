424d336b10dc1b1e0f9514311183287ea1945459
Sergey Fesenko <Sergey.Fesenko@aia.com>
2021-02-26 14:27:53 +0800

    update env vars

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "random_pet" "storage_account" {
   keepers = {
-    name     = var.name
-    location = var.location
+    name           = var.name
+    location       = var.location
+    resource_group = var.resource_group
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
+variable "resource_group" {
+  type    = string
+}
+
 variable "name" {
   type    = string
-  default = "demo"
 }
 
 variable "location" {
   type    = string
-  default = "East Asia"
 }
